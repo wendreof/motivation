b235156df13a239d6e89f8953b0ff0a4962595a6
wlf <wendreoofernandes@gmail.com>
2019-03-08 13:22:52 -0300

    Implementing filter

--- app/src/main/java/com/example/motivation/views/MainActivity.kt
+++ app/src/main/java/com/example/motivation/views/MainActivity.kt
@@ -2,12 +2,66 @@ package com.example.motivation.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.motivation.R
+import com.example.motivation.contants.MotivationConstants
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mFilter: Int = MotivationConstants.PRHASE_FILTER.ALL
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners() //events
+
+        handleFilter(R.id.imageAll) //initializing
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        val listId = listOf(R.id.imageAll, R.id.imageSun, R.id.imageHappy)
+        if (id in listId) {
+            handleFilter(id)
+        } else {
+            refreshPrase()
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        imageAll.setImageResource(R.drawable.ic_all_unselected)
+        imageSun.setImageResource(R.drawable.ic_sun_unselected)
+        imageHappy.setImageResource(R.drawable.ic_happy_unselected)
+
+        when (id) {
+            R.id.imageAll -> {
+                mFilter = MotivationConstants.PRHASE_FILTER.ALL
+                imageAll.setImageResource(R.drawable.ic_all_selected)
+            }
+            R.id.imageSun -> {
+                mFilter = MotivationConstants.PRHASE_FILTER.SUN
+                imageSun.setImageResource(R.drawable.ic_sun_selected)
+            }
+            R.id.imageHappy -> {
+                mFilter = MotivationConstants.PRHASE_FILTER.HAPPY
+                imageHappy.setImageResource(R.drawable.ic_happy_selected)
+            }
+        }
+    }
+
+
+    private fun refreshPrase() {
+    }
+
+
+    private fun setListeners() {
+        imageAll.setOnClickListener(this)
+        imageSun.setOnClickListener(this)
+        imageHappy.setOnClickListener(this)
+        buttonNewPhrase.setOnClickListener(this)
     }
 }

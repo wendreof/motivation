55ab46c58dc4de2ca5f7b62c5a0133847886edaa
wlf <wendreoofernandes@gmail.com>
2019-03-16 17:11:11 -0300

    Fix  synthetic property of Kotlin Android Extensions bug

--- app/src/main/java/com/example/motivation/views/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/views/SplashActivity.kt
@@ -1,11 +1,10 @@
 package com.example.motivation.views
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-
+import android.widget.Toast
 import com.example.motivation.R
 import com.example.motivation.contants.MotivationConstants
 import com.example.motivation.utils.SecurityPreferences
@@ -13,49 +12,46 @@ import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var mSecurity: SecurityPreferences
+    private lateinit var mSecurityPreferences: SecurityPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        mSecurity = SecurityPreferences(this)
+        mSecurityPreferences = SecurityPreferences( this )
 
         buttonSave.setOnClickListener(this)
-
         verifyUserName()
     }
 
-    private fun verifyUserName() {
-        val userName = mSecurity.getStoredString(MotivationConstants.KEY.PERSON_NAME)
-        if (userName != ""){
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-        editName.setText(userName)
-    }
-
-    override fun onClick(view: View) {
-        val id = view.id;
+    override fun onClick(view: View?) {
+        val id: Int? = view?.id
         if (id == R.id.buttonSave) {
             handleSave()
+        }
+    }
 
+    private fun verifyUserName() {
+        val name = mSecurityPreferences.getStoredString(MotivationConstants.KEY.PERSON_NAME)
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
     private fun handleSave() {
+
+        // Obtém o nome
         val name: String = editName.text.toString()
 
         if (name == "") {
-            Snackbar.make(splashActivity, getString(R.string.informe_nome), Snackbar.LENGTH_LONG).show()
+            Toast.makeText(this, "Informe seu nome.", Toast.LENGTH_LONG).show()
         } else {
-            mSecurity.storeString(MotivationConstants.KEY.PERSON_NAME, name)
 
-            val intent: Intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            mSecurityPreferences.storeString(MotivationConstants.KEY.PERSON_NAME, name)
+            startActivity(Intent(this, MainActivity::class.java))
 
-            //Impede a volta para essa activity!
-            //finish()
+            finish()
+
         }
-
     }
 }
